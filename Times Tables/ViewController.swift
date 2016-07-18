055cd9c41a833a1b20a30f856331f73b830268da
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-07-18 20:41:26 +0200

    Connecting slider and printing value into the log

--- Times Tables/ViewController.swift
+++ Times Tables/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sliderValue: UISlider!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        
+        print(sliderValue)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
